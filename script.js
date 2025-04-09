fac591a4641705b6e5c1ecfc8afd2fca299db22a
Catherine Y <catheriney@campus-014-138.ucdavis.edu>
2025-04-09 09:24:14 -0700

    update

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 (function () {
-    'use strict';
-  
-    const button = document.querySelector('button');
-    const html = document.documentElement;
-    const body = document.body;
-  
-    button.addEventListener('click', function () {
-      body.classList.toggle('dark-mode');
-      html.classList.toggle('dark-mode');
-    });
-  })();
-  
-  
+  'use strict';
+
+  const button = document.querySelector('button');
+  const html = document.documentElement;
+  const body = document.body;
+
+  button.addEventListener('click', function () {
+    body.classList.toggle('dark-mode');
+    html.classList.toggle('dark-mode');
+  });
+})();
+
