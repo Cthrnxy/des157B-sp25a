1056bf469c46de2776f87879bd2ace584abd1eda
Catherine Y <catheriney@campus-014-138.ucdavis.edu>
2025-04-09 09:21:02 -0700

    changed

--- script.js
+++ script.js
@@ -2,18 +2,13 @@
     'use strict';
   
     const button = document.querySelector('button');
+    const html = document.documentElement;
     const body = document.body;
-    const header = document.querySelector('header');
-    const sections = document.querySelectorAll('section');
   
     button.addEventListener('click', function () {
-      const isDark = body.classList.toggle('switch');
-  
-      header.classList.toggle('switch');
-      button.classList.toggle('switch');
-      sections.forEach(section => section.classList.toggle('switch'));
-  
-      button.textContent = isDark ? '🌞 Switch to Day' : '🌙 Switch to Night';
+      body.classList.toggle('dark-mode');
+      html.classList.toggle('dark-mode');
     });
   })();
+  
   
