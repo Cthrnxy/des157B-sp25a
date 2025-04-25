04860ffa606a34543cbcb411d5f69e3675d8a66e
Catherine Y <catheriney@CatherinedeMacBook-Air.local>
2025-04-25 15:27:03 -0700

    update check yourself

---
+++ checkYourself/script.js
@@ -0,0 +1,40 @@
+(function(){
+    "use strict"
+    console.log('reading js');
+
+    async function getData(){
+        const buttons=document.querySelectorAll('#table button');
+        const coffeeDiv=document.querySelector('#coffee');
+        //input data.json
+        const theData = await fetch('data.json');
+        const dataArray = await theData.json();
+        const cupsData = {};
+        dataArray.forEach(entry=>{
+            cupsData[entry.day]=entry,cups;
+        })
+
+        buttons.forEach(button=>{
+            button.addEventListener('mouseenter',()=>{
+                const day = button.getAttribute('data-day');
+                const cups= cupsData[day] || 0;
+
+                coffeeDiv.innerHTML=''; //clear the cups
+
+                for (let i=0; i<cups; i++){
+                    const img =document.querySelector('img');
+                    img.setAttribute('src', 'images/cup.PNG');
+                    coffeeDiv.appendChild(img);
+                }
+            });
+
+            button.addEventListener('mouseleave',()=>{
+                cups.forEach(cups=>{
+                    cups.addEventListener('animationend',()=>{
+                        cups.remove();
+                    })
+                })
+            })
+        })
+    }
+    getData();
+})();
